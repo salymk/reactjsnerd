03e72bd27641ec289ae3ff9789ce2beb4b96d31c
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-02-12 08:18:56 -0700

    Updated navbar
    Changed nav items to Gatsby Links and added active classname

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 const Navbar = () => (
-  <nav className="bg-gray-50 shadow">
+  <nav className="bg-white shadow">
     <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
       <div className="relative flex justify-between h-16">
         <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
@@ -62,34 +63,22 @@ const Navbar = () => (
           </div>
           <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
             {/* <!-- Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" --> */}
-            <a
-              href="#"
-              className="border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+            <Link
+              to="/"
+              className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+              activeClassName="active"
             >
               {' '}
               Blog{' '}
-            </a>
-            <a
-              href="#"
+            </Link>
+            <Link
+              to="/portfolio/"
               className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-md font-medium"
+              activeClassName="active"
             >
               {' '}
               Portfolio{' '}
-            </a>
-            <a
-              href="#"
-              className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-md font-medium"
-            >
-              {' '}
-              Contact{' '}
-            </a>
-            <a
-              href="#"
-              className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-md font-medium"
-            >
-              {' '}
-              Hire{' '}
-            </a>
+            </Link>
           </div>
         </div>
       </div>
@@ -103,30 +92,20 @@ const Navbar = () => (
     <div className="hidden sm:hidden">
       <div className="pt-2 pb-4 space-y-1">
         {/* <!-- Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" --> */}
-        <a
-          href="#"
+        <Link
+          to="/"
           className="bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-md font-medium"
+          activeClassName="active"
         >
           Blog
-        </a>
-        <a
-          href="#"
+        </Link>
+        <Link
+          to="/portfolio/"
           className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-md font-medium"
+          activeClassName="active"
         >
           Portfolio
-        </a>
-        <a
-          href="#"
-          className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-md font-medium"
-        >
-          Contact
-        </a>
-        <a
-          href="#"
-          className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-md font-medium"
-        >
-          Hire me
-        </a>
+        </Link>
       </div>
     </div>
   </nav>
