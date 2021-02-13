a96f59b5fbc826bb0e5aad5e19cd7bb3d6212d45
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-02-13 12:33:11 -0700

    Update navbar.js
    Made nav items font bigger

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -66,13 +66,13 @@ const Navbar = () => {
           </div>
           <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex-shrink-0 flex items-center text-xl font-bold text-indigo-700">
-              ReactJs Nerd
+              <Link to="/">ReactJs Nerd</Link>
             </div>
             <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
               {/* <!-- Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" --> */}
               <Link
                 to="/"
-                className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+                className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-lg font-medium"
                 activeClassName="active"
               >
                 {' '}
@@ -80,7 +80,7 @@ const Navbar = () => {
               </Link>
               <Link
                 to="/portfolio/"
-                className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-md font-medium"
+                className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-lg font-medium"
                 activeClassName="active"
               >
                 {' '}
