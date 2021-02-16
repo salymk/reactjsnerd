b550ba6b7dbfa80cf2a97f45b79dea0830af2951
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-02-15 18:17:12 -0700

    Update layout.js
    Added seo to layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,101 @@
 import React from 'react'
+import { SEOContext } from 'gatsby-plugin-wpgraphql-seo'
+import { useStaticQuery, graphql } from 'gatsby'
+
 import Navbar from './navbar'
 import Footer from './footer'
 
 // eslint-disable-next-line react/prop-types
-const Layout = ({ children }) => (
-  <div className="flex flex-col min-h-vh font-sans">
-    <Navbar />
-    <main className="mt-20">{children}</main>
-    <Footer />
-  </div>
-)
+const Layout = ({ children }) => {
+  const {
+    wp: { seo },
+  } = useStaticQuery(graphql`
+    query LayoutQuery {
+      wp {
+        generalSettings {
+          title
+          description
+        }
+        seo {
+          contentTypes {
+            post {
+              title
+              schemaType
+              metaRobotsNoindex
+              metaDesc
+            }
+            page {
+              metaDesc
+              metaRobotsNoindex
+              schemaType
+              title
+            }
+          }
+          webmaster {
+            googleVerify
+            yandexVerify
+            msVerify
+            baiduVerify
+          }
+          schema {
+            companyName
+            personName
+            companyOrPerson
+            wordpressSiteName
+            siteUrl
+            siteName
+            inLanguage
+            logo {
+              sourceUrl
+              mediaItemUrl
+              altText
+            }
+          }
+          social {
+            facebook {
+              url
+              defaultImage {
+                sourceUrl
+                mediaItemUrl
+              }
+            }
+            instagram {
+              url
+            }
+            linkedIn {
+              url
+            }
+            mySpace {
+              url
+            }
+            pinterest {
+              url
+              metaTag
+            }
+            twitter {
+              username
+            }
+            wikipedia {
+              url
+            }
+            youTube {
+              url
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <SEOContext.Provider value={{ global: seo }}>
+      <div className="flex flex-col min-h-vh font-sans">
+        <Navbar />
+        <main className="mt-20">{children}</main>
+        <Footer />
+      </div>
+    </SEOContext.Provider>
+  )
+}
 
 export default Layout
