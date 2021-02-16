c248196e3806aea2afb12b0c1a6b7b9f5882678f
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-02-15 18:17:05 -0700

    Update blog-post.js
    Added seo to every post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,22 +4,54 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import parse from 'html-react-parser'
 import Img from 'gatsby-image'
+import Seo from 'gatsby-plugin-wpgraphql-seo'
+import { v4 as uuidv4 } from 'uuid'
+
 import Layout from '../components/layout'
 
 export default function BlogPost({ data }) {
   const post = data.allWpPost.nodes[0]
-  console.log(post.featuredImage.node.localFile.childImageSharp.fluid)
+  // console.log(post.featuredImage.node.localFile.childImageSharp.fluid)
+
+  const tagColor = tagName => {
+    if (tagName === 'reactjs') {
+      return 'bg-indigo-100 text-indigo-800'
+    }
+    if (tagName === 'epicreactdev') {
+      return 'bg-gray-100 text-gray-800'
+    }
+    if (tagName === 'gatsbyjs') {
+      return 'bg-purple-100 text-purple-800'
+    }
+  }
   return (
     <Layout>
+      <Seo post={post} />
       <div className="py-16 bg-white overflow-hidden">
         <div className="px-4 sm:px-6 lg:px-8">
           <div className="max-w-2xl mx-auto">
             <Img
               className="w-full h-full object-cover mb-12"
               fluid={post.featuredImage.node.localFile.childImageSharp.fluid}
+              alt={post.featuredImage.node.localFile.alt}
             />
           </div>
+
           <div className="text-lg max-w-prose mx-auto">
+            <div className="max-w-2xl">
+              <div className="mx-auto">
+                {post.tags.nodes.map(tag => (
+                  <span
+                    key={uuidv4()}
+                    className={`mr-2 inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium ${tagColor(
+                      tag.name
+                    )} `}
+                  >
+                    {tag.name}{' '}
+                  </span>
+                ))}
+              </div>
+            </div>
             <h1>
               <span className="mt-2 block text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-3xl">
                 {post.title}
@@ -42,6 +74,40 @@ export const query = graphql`
       nodes {
         title
         content
+        tags {
+          nodes {
+            name
+          }
+        }
+        seo {
+          title
+          metaDesc
+          focuskw
+          metaKeywords
+          metaRobotsNoindex
+          metaRobotsNofollow
+          opengraphTitle
+          opengraphDescription
+          opengraphImage {
+            altText
+            sourceUrl
+            srcSet
+          }
+          twitterTitle
+          twitterDescription
+          twitterImage {
+            altText
+            sourceUrl
+            srcSet
+          }
+          canonical
+          cornerstone
+          schema {
+            articleType
+            pageType
+            raw
+          }
+        }
         featuredImage {
           node {
             altText
