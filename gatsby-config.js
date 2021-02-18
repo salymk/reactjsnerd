606b1eca92421551818008e2857568b0c77d8de8
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-02-18 13:34:15 -0700

    New tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,8 +19,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: `G-S15YKPFN7Y`,
-        head: true,
+        // The property ID; the tracking code won't be generated without it
+        trackingId: '262572617',
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
         anonymize: true,
       },
     },
